ed7cb03975874dc00d0e49fd09817a13f09fc2a6
Reynard van den Berg <reyvdb@gmail.com>
2020-01-11 21:11:49 +0800

    add styling to header

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :events, only: [:new, :create, :show, :index]
 
+  root 'events#index'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
